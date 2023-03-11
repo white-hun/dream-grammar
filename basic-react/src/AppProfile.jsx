009de76bef9984886a3863b1576221e756f49632
white-hun <121800818+white-hun@users.noreply.github.com>
2023-03-11 23:42:52 +0900

    Update component separation

--- basic-react/src/AppProfile.jsx
+++ basic-react/src/AppProfile.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import Avatar from "./components/Avatar";
 import Profile from "./components/Profile";
 
 export default function AppProfile() {
   return (
     <>
+      <Avatar
+        img="https://images.unsplash.com/photo-1678373978945-acff8f875465?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"
+        isNew={true}
+      />
       <Profile
         img="https://images.unsplash.com/photo-1678465952838-c9d7f5daaa65?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60"
         name="White"
--- basic-react/src/components/Profile.jsx
+++ basic-react/src/components/Profile.jsx
@@ -1,8 +1,9 @@
+import Avatar from "./Avatar";
+
 export default function Profile({ img, name, title, isNew }) {
   return (
     <div className="profile">
-      <img className="photo" src={img} alt="avatar" />
-      {isNew && <span className="state">new</span>}
+      <Avatar img={img} isNew={isNew} />
       <h1>{name}</h1>
       <p>{title}</p>
     </div>
