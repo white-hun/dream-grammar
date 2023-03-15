634ad872071e94560932946d5c87e162e2345889
white-hun <121800818+white-hun@users.noreply.github.com>
2023-03-15 11:21:33 +0900

    Update practice

--- basic-react/src/AppMentors.jsx
+++ basic-react/src/AppMentors.jsx
@@ -172,35 +172,115 @@
 
 //---------------------------------------------------------------------------------
 
+// import React, { useState } from "react";
+
+// export default function AppMentors() {
+//   const [person, setPerson] = useState(initialPerson);
+//   const handleUpdate = () => {
+//     const previous = prompt(`Whose name do you want to change?`);
+//     const current = prompt(`What do you want to change your name to?`);
+//     setPerson((prev) => ({
+//       ...prev,
+//       mentors: prev.mentors.map((mentor) => {
+//         return mentor.name === previous ? { ...mentor, name: current } : mentor;
+//       }),
+//     }));
+//   };
+//   const hanbleAdd = () => {
+//     const name = prompt(`Write the name of the mentor you want to add.`);
+//     const title = prompt(`Write the title of the mentor you want to add.`);
+//     setPerson((prev) => ({
+//       ...prev,
+//       mentors: [...prev.mentors, { name, title }],
+//     }));
+//   };
+//   const hanbleDelete = () => {
+//     const deleteMentor = prompt(
+//       `Write the name of the mentor you want to delete.`
+//     );
+//     setPerson((prev) => ({
+//       ...prev,
+//       mentors: prev.mentors.filter((mentor) => mentor.name !== deleteMentor),
+//     }));
+//   };
+//   return (
+//     <div>
+//       <h1>
+//         {person.name} is {person.title}
+//       </h1>
+//       <p>{person.name}'s mentoris:</p>
+//       <ul>
+//         {person.mentors.map((mentor, index) => (
+//           <li key={index}>
+//             {mentor.name} ({mentor.title})
+//           </li>
+//         ))}
+//       </ul>
+//       <button onClick={handleUpdate}>Change the Mentor Name</button>
+//       <button onClick={hanbleAdd}>Add the Mentor</button>
+//       <button onClick={hanbleDelete}>Delete the Mentor</button>
+//     </div>
+//   );
+// }
+
+// const initialPerson = {
+//   name: "white",
+//   title: "Beginner",
+//   mentors: [
+//     {
+//       name: "Ellie",
+//       title: "Senior Developer",
+//     },
+//     {
+//       name: "Bob",
+//       title: "Full Stack Developer",
+//     },
+//   ],
+// };
+//---practice---------------------------------------------------
+
 import React, { useState } from "react";
 
 export default function AppMentors() {
   const [person, setPerson] = useState(initialPerson);
-  const handleUpdate = () => {
-    const previous = prompt(`Whose name do you want to change?`);
-    const current = prompt(`What do you want to change your name to?`);
-    setPerson((prev) => ({
-      ...prev,
-      mentors: prev.mentors.map((mentor) => {
-        return mentor.name === previous ? { ...mentor, name: current } : mentor;
-      }),
-    }));
-  };
-  const hanbleAdd = () => {
-    const name = prompt(`Write the name of the mentor you want to add.`);
-    const title = prompt(`Write the title of the mentor you want to add.`);
-    setPerson((prev) => ({
-      ...prev,
-      mentors: [...prev.mentors, { name, title }],
-    }));
-  };
-  const hanbleDelete = () => {
-    const deleteMentor = prompt(
-      `Write the name of the mentor you want to delete.`
+  const changeClick = () => {
+    const previousMentorName = prompt(`Whose name do you want to change?`);
+    const currentMentorName = prompt(
+      `What do you want to change your name to?`
     );
     setPerson((prev) => ({
       ...prev,
-      mentors: prev.mentors.filter((mentor) => mentor.name !== deleteMentor),
+      mentors: prev.mentors.map((mentor) => {
+        return mentor.name === previousMentorName
+          ? { ...mentor, name: currentMentorName }
+          : mentor;
+      }),
+    }));
+  };
+  const AddClick = () => {
+    const AddMentorName = prompt(
+      `Write the name of the mentor you want to add.`
+    );
+    const AddMentorTitle = prompt(
+      `Write the title of the mentor you want to add.`
+    );
+    setPerson((prev) => ({
+      ...prev,
+      mentors: [
+        ...prev.mentors,
+        { name: AddMentorName, title: AddMentorTitle },
+      ],
+    }));
+  };
+  const deleteClick = () => {
+    const deleteMentor = prompt(
+      `Write the name of the mentor you want to delete`
+    );
+    setPerson((prev) => ({
+      ...prev,
+      mentors: prev.mentors.filter((mentor) => {
+        return mentor.name !== deleteMentor;
+      }),
     }));
   };
   return (
@@ -208,7 +288,7 @@ export default function AppMentors() {
       <h1>
         {person.name} is {person.title}
       </h1>
-      <p>{person.name}'s mentoris:</p>
+      <p>{person.name}'s mentor is:</p>
       <ul>
         {person.mentors.map((mentor, index) => (
           <li key={index}>
@@ -216,9 +296,9 @@ export default function AppMentors() {
           </li>
         ))}
       </ul>
-      <button onClick={handleUpdate}>Change the Mentor Name</button>
-      <button onClick={hanbleAdd}> Add the Mentor</button>
-      <button onClick={hanbleDelete}>Delete the Mentor</button>
+      <button onClick={changeClick}>Change the Mentor Name</button>
+      <button onClick={AddClick}>Add the Mentor</button>
+      <button onClick={deleteClick}>Delete the Mentor</button>
     </div>
   );
 }
